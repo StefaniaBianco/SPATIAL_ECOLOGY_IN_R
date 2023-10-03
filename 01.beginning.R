ee2607c9b8e832c3fd870ee7e1010292969123c2
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2023-10-03 17:04:26 +0200

    Create 01.beginning.R

---
+++ 01.beginning.R
@@ -0,0 +1,20 @@
+#Here I can write anything I want, since it is a comment for R
+##for every operation there should be an explanation. That's why we should comment our R code
+
+#R as a calculator
+2+3
+
+#assign to an object
+zima <- 2+3
+zima
+[1] 5
+> duccio <- 5+6
+> duccio
+[1] 11
+> zima*duccio
+[1] 55
+> final <- zima*duccio
+> final
+[1] 55
+> final^2
+[1] 3025
