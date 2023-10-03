0d7af5dffd5686318628f5d88b6d6eb86f06469d
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2023-10-03 17:55:46 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -18,3 +18,24 @@ zima
 [1] 55
 > final^2
 [1] 3025
+
+#array: series of information all together
+sophi <- c (10, 20, 30, 50, 50) #microplastics
+#c is a function, has parentheses and inside there are arguments
+paula <- c(100, 500, 600, 1000, 2000) #people
+#we can make a plot with sophi (y) and paula (x)
+plot(paula, sophi)
+#direcly correlation between amount of people and microplastics
+> #if i want to put a new label instaed of paula and sophi
+> plot(paula, sophi, xlab=number of people, ylab=microplastics)
+Error: unexpected symbol in "plot(paula, sophi, xlab=number of"
+> plot(paula, sophi, xlab="number of people", ylab="microplastics")
+> #another way
+> people <- paula
+> microplastics <- sophi
+> plot(people, microplastics, pch=19) #look on internet the point symbols in r
+ plot(people, microplastics, pch=19, cex=2) #character exageration
+plot(people, microplastics, pch=19, cex=2, col="blue") #change the colour, remember the quotes
+
+> install.packages("sp") #we need quotes 
+> library(sp) #to check
