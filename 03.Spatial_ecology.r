157b4ae6ac2398537503db0fdd1b48ad7d48f7ef
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2023-11-09 17:05:50 +0100

    Update 03.Spatial_ecology.r

--- 03.Spatial_ecology.r
+++ 03.Spatial_ecology.r
@@ -48,3 +48,46 @@ plot(densitymap)
 plot(elev)
 #we understand the highest elevation corrisponds to less tree and lowest density
 par(mfrow=c(2,1))
+
+
+library(imageRy)
+library(terra)
+
+im.list()
+m1992<-im.import("matogrosso_l5_1992219_lrg.jpg")
+#bands 1=NIR, 2=RED, 3=GREEN
+im.plotRGB(m1992,r=2,g=1,b=3)
+im.plotRGB(m1992,r=2,g=3,b=1) #now bare soil and the river is yellow
+
+m2006<-im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006,r=2,g=3,b=1)
+
+#let's build a multiframe with 1992 and 2006
+par(mfrow=c(1,2))
+im.plotRGB(m1992,r=2,g=3,b=1)
+im.plotRGB(m2006,r=2,g=3,b=1)
+
+#let's create vegetation index to see the health state
+dev.off()
+plot(m1992[[1]]) #let's plot the NIR band
+#the range goes from 0 to 255. this is the reflectance
+#reflectance=reflected flux of energy/incidence flux of energy
+#we don't use this formula as it is a decimal number
+#we use bit to reschedule. one bit of info is 0 or 1 (binary code)
+#with one bit you can have 2 info, with 2 bits 4 info and so on
+#normally there are info stored in 8 bit (2^8=256 information)
+#we want to make the difference between the first band of 1992 and the second band of 1992, so we will get the DVI=NIR-RED
+#let's make DVI of 1992
+dvi1992 = m1992[[1]]-m1992[[2]]
+#in this way we can plot 
+plot(dvi1992)
+#we have for each pixel the difference between the two bands
+#we can see the amount of healthy vegetation. everything in green in healthy forest, that reflect the NIR and absorde the RED(explain better this concept!!!)
+cl<-colorRampPalette(c("darkblue","yellow","red", "black"))(100)
+plot(dvi1992, col=cl)
+
+#let's do the same for 2006
+plot(m2006)
+dvi2006 = m2006[[1]]-m2006[[2]]
+plot(dvi2006, col=cl)
+#we see how healty vegetation is a very small amount, as deforestation is destroying the vegetation. 
