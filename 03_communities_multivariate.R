d09bc0cec938ef98f53992624f62f614487a9719
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2023-12-13 12:16:44 +0100

    Update and rename 03_communities_multivariate to 03_communities_multivariate.R

--- 03_communities_multivariate.R
+++ 03_communities_multivariate.R
@@ -10,44 +10,34 @@ data(dune)
 dune #we have a matrix of several data all together
 head(dune) #to see the first six rows of the samples
 
-#library(vegan)
-install.packages("overlap")
-library(overlap)
-
-#to recall the data from our dataset, we use the function data()
-data(dune)
-dune #we have a matrix of several plots all together
-head(dune) #to see the first six rows of the samples
-
 #we can use the "detrended correspondence analysis" to see how species are correlated between each other in a plot. 
-#this also helps us to reshape the data in a simpler mannaer
+#this also helps us to reshape the data in a simpler manner
+#to do this, we use the function decorana ()
 ord<-decorana(dune)
 ord
 
-#we need to understand the length of the new axes.
-
+#on the console there are displayed the values of "axis lenght" 
 ldc1=3.7004 
 ldc2=3.1166
 ldc3=1.30055 
 ldc4=1.47888
 
+#and the total is given by
 total<-ldc1+ldc2+ldc3+ldc4
 total
 
-#let's calculate the percentage
-
+#let's calculate the percentage of each axes lenght compared to the total
 pldc1=ldc1*100/total
 pldc2=ldc2*100/total
 pldc3=ldc3*100/total
 pldc4=ldc4*100/total
-
 pldc1
 pldc2
 pldc3
 pldc4
 
-#we see 1 and 2 that together make more than 70% so we 
+pldc1 + pldc2 #we see 1 and 2 that together make more than 70% of the total 
 
-plot(ord)
-#here we see how different species are related to each other, staying together in any part
+#final plot
+plot(ord) #here we see how different species are related (or not) to each other
 
