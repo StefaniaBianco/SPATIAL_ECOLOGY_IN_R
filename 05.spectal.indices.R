ad33bcfa6c201bd0ea7ac2d4c5981ae20744e02f
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2023-12-18 21:44:01 +0100

    Update 05.spectal.indices.R

--- 05.spectal.indices.R
+++ 05.spectal.indices.R
@@ -40,21 +40,23 @@ im.plotRGB(m1992,r=2,g=3,b=1)
 im.plotRGB(m2006,r=2,g=3,b=1)
 dev.off()
 
-#let's use the vegetation index to see the health state
-plot(m1992[[1]]) #let's plot the NIR band
-#the range goes from 0 to 255. this is the reflectance
+#let's use the vegetation index to see the health state (DVI)
+plot(m1992[[1]]) #in this way we plot the NIR band
+#in this case, the range of the reflectance goes from 0 to 255
+
 #reflectance=reflected flux of energy/incidence flux of energy
-#we don't use this formula as it is a decimal number
+#we don't use this formula as it gives as a decimal number as a result
 #we use bit to reschedule. one bit of info is 0 or 1 (binary code)
 #with one bit you can have 2 info, with 2 bits 4 info and so on
-#normally there are info stored in 8 bit (2^8=256 information)
+#normally the information are stored in 8 bit (2^8=256 information)
+
 #we want to make the difference between the first band of 1992 and the second band of 1992, so we will get the DVI=NIR-RED
 #let's make DVI of 1992
 dvi1992 = m1992[[1]]-m1992[[2]]
 #in this way we can plot 
 plot(dvi1992)
 #we have for each pixel the difference between the two bands
-#we can see the amount of healthy vegetation. everything in green in healthy forest, that reflect the NIR and absorde the RED(explain better this concept!!!)
+#we can see the amount of healthy vegetation. everything in green is healthy forest
 cl<-colorRampPalette(c("darkblue","yellow","red", "black"))(100)
 plot(dvi1992, col=cl)
 
@@ -62,17 +64,7 @@ plot(dvi1992, col=cl)
 plot(m2006)
 dvi2006 = m2006[[1]]-m2006[[2]]
 plot(dvi2006, col=cl)
-#we see how healty vegetation is a very small amount, as deforestation is destroying the vegetation. 
-
-
-cl<-colorRampPalette(c("darkblue","yellow","red", "black"))(100)
-plot(dvi1992, col=cl)
-
-#let's do the same for 2006
-plot(m2006)
-dvi2006 = m2006[[1]]-m2006[[2]]
-plot(dvi2006, col=cl)
-#we see how healty vegetation is a very small amount, as deforestation is destroying the vegetation. 
+#we see now healty vegetation is a very small amount, as deforestation is destroying the forest 
 
 #what if we have images with different bits?
 #we have to standardize the bit (normalized)
@@ -86,22 +78,21 @@ ndvi1992 = dvi1992 / (m1992[[1]]+m1992[[2]])
 plot(ndvi1992, col=cl)
 #the dark red part is the healthy vegetation
 ndvi2006 = dvi2006 / (m2006[[1]]+m2006[[2]])
-plot(ndvi2006, col=cl)
-
+plot(ndvi2006, col=cl) 
 dev.off()
+
+#let's create a multiframe to compare the two images
 par(mfrow=c(1,2))
 plot(ndvi1992, col=cl)
 plot(ndvi2006, col=cl)
+#we see the situation in 2006 is way worse
 
 #to be inclusive for daltonic
 clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
 plot(ndvi2006, col=clvir)
 plot(ndvi1992, col=clvir)
 
-#speeding up the calculation, we can use the function from imageRy package
+#if we want to speed up the calculation, we can use the function from imageRy package
 ndvi2006a<-im.ndvi(m2006, 1, 2)
 plot(ndvi2006, col=cl)
-
-
-
 
