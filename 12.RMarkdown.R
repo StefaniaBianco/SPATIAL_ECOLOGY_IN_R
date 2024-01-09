416e2e3c7f1a3954b685b16f52f9c5bbd5fe88f3
StefaniaBianco <146854140+StefaniaBianco@users.noreply.github.com>
2024-01-09 17:39:28 +0100

    Create 12.RMarkdown.R

---
+++ 12.RMarkdown.R
@@ -0,0 +1,20 @@
+---
+title: "MarkdownReport"
+author: "Stefania Bianco"
+date: "2024-01-09"
+output: html_document
+---
+
+# My first markdown document
+
+Here you can add some text if you want
+
+In this case, you obtain the result
+```{r, eval=T}
+2+3
+```
+
+In this case, you do not obtain the result
+```{r, eval=F}
+2+3
+```
